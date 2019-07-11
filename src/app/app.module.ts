4d3fca2c1aaa6d94b01b787c4eb7bec701ebdcdd
Jesus Valentin Mendoza Torres <jmendoto@everis.com>
2019-07-10 22:27:42 -0500

    add initial files template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './ui/mycv/profile/profile.component';
+import { LayoutComponent } from './ui/layout/layout.component';
+import { ExperienceComponent } from './ui/mycv/experience/experience.component';
+import { ToolsComponent } from './ui/mycv/tools/tools.component';
+import { MycvComponent } from './ui/mycv/mycv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LayoutComponent,
+    ExperienceComponent,
+    ToolsComponent,
+    MycvComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ui/mycv/mycv.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mycv',
+  templateUrl: './mycv.component.html'
+})
+export class MycvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
